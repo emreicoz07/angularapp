f0fa02668317a4ee12443f693064e388fbf46082
emreicoz07 <ramazanemreicoz@gmail.com>
2024-12-10 14:14:56 +0100

    pages are ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ]
 })
 export class AppComponent {
-  title = 'test-app';
-}
+  title = "Emre`s` App"
+ }
